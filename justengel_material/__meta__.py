358b572f0969b66e9497ea4b0405d8d6d86be808
justengel <jtengel08@gmail.com>
2020-07-07 23:58:54 -0400

    Updated to version 1.0.0

--- justengel_material/__meta__.py
+++ justengel_material/__meta__.py
@@ -1,5 +1,5 @@
 name = 'justengel_material'
-version = '0.0.3'
+version = '1.0.0'
 description = 'Jinja Materializecss theme capability'
 url = 'https://github.com/justengel-web/justengel_material'
 author = 'Justin Engel'
